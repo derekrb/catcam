0dde814452d82e06a8f40e67812ae39f68cc6d77
derekrb <derekrb@gmail.com>
2016-12-28 14:52:24 -0500

    test video

--- app/api.py
+++ app/api.py
@@ -1,6 +1,15 @@
+import subprocess
+
 from app import app
 
 
 @app.route('/ping')
 def index():
+    return 'OK', 200
+
+
+@app.route('/video')
+def video():
+    cmd = ['omxplayer', '-b', '-o', 'hdmi', '/opt/vc/src/hello_pi/hello_video/test.h264']
+    subprocess.check_call(cmd)
     return 'OK', 200
